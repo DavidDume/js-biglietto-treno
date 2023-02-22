97a4602a6ab65c75cc7366f41b949b1ab553e4e7
David Dume <dumedavid11@gmail.com>
2023-02-22 15:57:10 +0100

    controllo se il prompt è un numero

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const km = parseInt(prompt("Quanto è lungo il viaggio? in KM"));
 const eta = parseInt(prompt("Qual'è l'età del passeggero?"));
 
-let costo = 0.21 * km;
+if(isNaN(km) || isNaN(eta)) {
+    console.log('Insersci un numero valido!');
+} else {
+    let costo = 0.21 * km;
 
-if(eta < 18) {
-    costo = (costo * 20) / 100;
-} else if(eta >= 65) {
-    costo = (costo * 40) / 100;
+    if(eta < 18) {
+        costo = (costo * 20) / 100;
+    } else if(eta >= 65) {
+        costo = (costo * 40) / 100;
+    }
+    
+    console.log(costo.toFixed(2));
 }
-
-console.log(costo.toFixed(2));
